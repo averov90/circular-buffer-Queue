8bf5a8444dea154058ba99c25cbfd8d97d20fa8a
averov90 <averov90@inbox.ru>
2021-05-08 04:40:28 +0300

    Added Circular Buffer class

---
+++ CircularBuffer.h
@@ -0,0 +1,378 @@
+﻿#pragma once
+
+#include <cstring>
+#include <ostream>
+#include <stdexcept>
+#include <iterator>
+
+template <typename T>
+class CircularBuffer {
+	T *items, *itemse, *pick, *place;
+	unsigned int capacitance, stored_count;
+
+public:
+	CircularBuffer(unsigned int capacity):capacitance(capacity) {
+		if (!capacity) throw std::invalid_argument("Capacity must be at least 1!");
+
+		pick = place = items = reinterpret_cast<T *>(malloc(capacity * sizeof(T)));
+		if (!items) throw std::runtime_error("You don't have enough memory!");
+
+		stored_count = 0;
+		itemse = items + capacity;
+	}
+
+	CircularBuffer(const std::initializer_list<T> &list): capacitance(list.size()) {
+		if (!capacitance) throw std::invalid_argument("Capacity must be at least 1!");
+		stored_count = capacitance;
+
+		pick = place = items = reinterpret_cast<T *>(malloc(capacitance * sizeof(T)));
+		if (!items) throw std::runtime_error("You don't have enough memory!");
+		itemse = items + capacitance;
+
+		{
+			const T *citem = list.begin();
+			for (T *cwpos = items; cwpos != itemse; ++cwpos, ++citem)
+				*cwpos = *citem;
+		}
+	}
+
+	CircularBuffer(const CircularBuffer &arg): capacitance(arg.capacitance), stored_count(arg.stored_count) {
+		items = reinterpret_cast<T *>(malloc(capacitance * sizeof(T)));
+		if (!items) throw std::runtime_error("You don't have enough memory!");
+		itemse = items + capacitance;
+
+		place = items + (arg.place - arg.items);
+		pick = items + (arg.pick - arg.items);
+
+		memcpy(items, arg.items, capacitance * sizeof(T));
+	}
+
+	CircularBuffer &operator=(const CircularBuffer &arg) {
+		capacitance = arg.capacitance;
+		stored_count = arg.stored_count;
+		
+		items = reinterpret_cast<T *>(malloc(capacitance * sizeof(T)));
+		if (!items) throw std::runtime_error("You don't have enough memory!");
+		itemse = items + capacitance;
+
+		place = items + (arg.place - arg.items);
+		pick = items + (arg.pick - arg.items);
+
+		memcpy(items, arg.items, capacitance * sizeof(T));
+
+		return *this;
+	}
+
+	~CircularBuffer() {
+		free(items);
+	}
+
+	void emplace(const T &item) {
+		if (place == pick) { // Common case
+			if (stored_count) { //Ring is full, rewriting
+				new (place) T(item); //Assigning an item to a memory address.
+				if (++pick == itemse) {
+					pick = items;
+				}
+				place = pick;
+			} else { // Ring is empty, all is ok
+				new (place) T(item); //Assigning an item to a memory address.
+				++stored_count;
+				if (++place == itemse) {
+					place = items;
+				}
+			}
+		} else {
+			new (place) T(item); //Assigning an item to a memory address.
+			++stored_count;
+			if (++place == itemse) {
+				place = items;
+			}
+		}
+	}
+
+	void emplace(unsigned int index, const T &item) {
+		if (index >= stored_count) throw std::out_of_range("Index out of range!");
+
+		unsigned int must_steps = stored_count - index;
+
+		T *back = place, *front;
+
+		//Getting empty place
+		if (place == pick) {
+			if (++pick == itemse) {
+				pick = items;
+			}
+			--must_steps; //It is necessary to account for the change in index from the deletion of the oldest element
+		} else
+			++stored_count;
+
+		if (++place == itemse) {
+			place = items;
+		}
+
+		//Shifting items
+		for (unsigned int cstep = 0; cstep != must_steps; ++cstep) {
+			front = back;
+			if (--back < items) {
+				back = itemse - 1;
+			}
+			*front = *back;
+		}
+		
+		//Emplacing
+		new (back) T(item);
+	}
+
+	void erase(unsigned int index) {
+		if (index >= stored_count) throw std::out_of_range("Index out of range!");
+
+		T *back, *front = pick;
+
+		--stored_count;
+
+		//Moving to needed item
+		for (unsigned int cstep = 0; cstep != index; ++cstep) {
+			if (++front == itemse) {
+				front = items;
+			}
+		}
+
+		if (--place < items) {
+			place = itemse - 1;
+		}
+
+		//Shifting items
+		while (front != place) 	{
+			back = front;
+			if (++front == itemse) {
+				front = items;
+			}
+			*back = *front;
+		}
+	}
+
+	unsigned int capacity() const {
+		return capacitance;
+	}
+	
+	void capacity(unsigned int new_capacity) {
+		if (stored_count > new_capacity) throw std::out_of_range("The capacity of the structure cannot be less than the number of elements in it!");
+		if (new_capacity > capacitance) {
+			T *new_pointer = reinterpret_cast<T *>(realloc(items, new_capacity * sizeof(T)));
+			if (!new_pointer) throw std::runtime_error("You don't have enough memory!");
+
+			unsigned int epic_offset = place - items;
+			items = new_pointer;
+
+			if (epic_offset) {
+				memcpy(pick = new_pointer + new_capacity - capacitance + epic_offset, new_pointer + epic_offset, (capacitance - epic_offset) * sizeof(T));
+				place = new_pointer + epic_offset;
+			} else {
+				place = new_pointer + capacitance;
+				pick = new_pointer;
+			}
+			capacitance = new_capacity;
+			itemse = items + capacitance;
+		} else {
+			T *new_pointer = reinterpret_cast<T *>(malloc(new_capacity * sizeof(T)));
+			if (!new_pointer) throw std::runtime_error("You don't have enough memory!");
+
+			if (place > pick) {
+				memcpy(new_pointer, pick, stored_count * sizeof(T));
+			} else {
+				memcpy(new_pointer, pick, (itemse - pick) * sizeof(T));
+				memcpy(new_pointer + (itemse - pick), items, (place - items) * sizeof(T));
+			}
+			pick = new_pointer;
+			if (new_capacity == stored_count)
+				place = new_pointer;
+			else
+				place = new_pointer + stored_count;
+
+			capacitance = new_capacity;
+
+			free(items); items = new_pointer;
+			itemse = items + capacitance;
+		}
+	}
+
+	void insert(unsigned int index, T item) {
+		const T &ref = item;
+		emplace(index, item);
+	}
+
+	void push(T item) {
+		const T &ref = item;
+		emplace(item);
+	}
+
+	T pop() {
+		if (stored_count) {
+			T *item = pick;
+			--stored_count;
+			if (++pick == itemse) {
+				pick = items;
+			}
+			return *item;
+		} else
+			throw std::underflow_error("Nothing to Pop!");
+	}
+
+	unsigned int count() const {
+		return stored_count;
+	}
+
+
+	bool operator==(const CircularBuffer &arg) const {
+		if (arg.stored_count != stored_count)
+			return false;
+
+		if (stored_count) {
+			T *pk = pick, *em = place;
+			T *pk2 = arg.pick, *em2 = arg.place;
+
+			do {
+				if (*pk != *pk2)
+					return false;
+
+				if (++pk == itemse)
+					pk = items;
+				if (++pk2 == arg.itemse)
+					pk2 = arg.items;
+			} while (pk != em && pk2 != em2);
+		}
+
+		return true;
+	}
+
+	bool operator>(const CircularBuffer &arg) const {
+		if (stored_count) {
+			T *pk = pick, *em = place;
+			T *pk2 = arg.pick, *em2 = arg.place;
+
+			while (true) {
+				if (*pk > *pk2)
+					return true;
+
+				if (++pk == itemse)
+					pk = items;
+				if (pk == em) return false;
+				if (++pk2 == arg.itemse)
+					pk2 = arg.items;
+				if (pk2 == em2) return true;
+			}
+		}
+
+		return false;
+	}
+
+	//Just to be short. If necessary, you can make a separate implementation for each operator (like for == and >), which will speed them up.
+	bool operator!=(const CircularBuffer &arg) const {
+		return !(*this == arg);
+	}
+
+	bool operator>=(const CircularBuffer &arg) const {
+		return *this > arg || *this == arg;
+	}
+
+	bool operator<=(const CircularBuffer &arg) const {
+		return !(*this > arg);
+	}
+
+	bool operator<(const CircularBuffer &arg) const {
+		return *this <= arg && *this != arg;
+	}
+
+	class iterator {
+		friend class CircularBuffer;
+
+		unsigned int index, err_index;
+		T *ptr, *base, *end;
+		iterator(T *current, T *base, T *end, unsigned int index, unsigned int err_index): ptr(current), base(base), end(end), index(index), err_index(err_index){ }
+	public:
+		using iterator_category = std::bidirectional_iterator_tag;
+		using value_type = T;
+		using difference_type = std::ptrdiff_t;
+		using pointer = T*;
+		using reference = T&;
+		//iterator(const iterator &it); //Not need
+		//iterator &operator=(iterator const &other);
+		
+		unsigned int get_index() const {
+			return index;
+		}
+
+		bool operator!=(iterator const &other) const {
+			return index != other.index;
+		}
+
+		bool operator==(iterator const &other) const {
+			return index == other.index;
+		}
+
+		T &operator*() const {
+			if (index == err_index) throw std::logic_error("Can not dereference end iterator!");
+			return *ptr;
+		}
+
+		T *operator->() const {
+			if (index == err_index) throw std::logic_error("Can not dereference end iterator!");
+			return ptr;
+		}
+
+		iterator &operator++() {
+			if (index == err_index) throw std::out_of_range("Iterator out of range!");
+			++index;
+			if (++ptr == end) {
+				ptr = base;
+			}
+			return *this;
+		}
+
+		iterator operator++(int) {
+			if (index == err_index) throw std::out_of_range("Iterator out of range!");
+			iterator temp(*this);
+			++index;
+			if (++ptr == end) {
+				ptr = base;
+			}
+			return temp;
+		}
+
+		iterator &operator--() {
+			if (index == 0) throw std::out_of_range("Iterator out of range!");
+			--index;
+			if (--ptr < base) {
+				ptr = end - 1;
+			}
+			return *this;
+		}
+
+		iterator operator--(int) {
+			if (index == 0) throw std::out_of_range("Iterator out of range!");
+			iterator temp(*this);
+			--index;
+			if (--ptr < base) {
+				ptr = end - 1;
+			}
+			return temp;
+		}
+	};
+
+	iterator begin() const {
+		return iterator(pick, items, itemse, 0, stored_count);
+	}
+
+	iterator end() const {
+		return iterator(place, items, itemse, stored_count, stored_count);
+	}
+};
+
+template<typename T>
+std::ostream &operator<<(std::ostream &out, const CircularBuffer<T> &buffer) {
+	for (auto item : buffer) {
+		out << item << std::endl;
+	}
+	return out;
+}
--- README-eng.md
+++ README-eng.md
@@ -1,4 +1,5 @@
 # circular buffer Queue
+## Circular Buffer
 [![License](https://img.shields.io/badge/LICENSE-The%20Unlicense-green?style=flat-square)](/LICENSE)  [![Version](https://img.shields.io/badge/VERSION-STABLE-green?style=flat-square)](https://github.com/averov90/circular-buffer-Queue/releases)
 ### :small_orange_diamond: [Russian version](/README.md)
 
@@ -13,4 +14,11 @@ This solution will avoid using the potentially expensive *memcpy* for a large nu
 It is also possible to optimize the reversal of the direction of the queue.
 This can be done by swapping the start and end pointers, as well as adding a new field that will store the direction of traversing the elements of the ring.
 
-These are all pretty useful and interesting tasks on the topic of coding.
+#### A ring buffer class was implemented based on structure from Queue.h
+
+The main difference in the way it works is that *CircularBuffer.h* cannot change the overflow capacity - it just replaces the oldest elements.
+However, several additional functions have been added to the *CircularBuffer.h* class (*insert* and *erase*, which allow adding and deleting arbitrary elements), as well as full support for iteration.
+Multi-pass iteration is implemented both forward and backward. The iterator has a category *bidirectional_iterator_tag* (includes the categories *forward_iterator_tag*, *output_iterator_tag*, *input_iterator_tag*).
+The "new" approach of creating an iterator was used, when inheritance from *std::iterator* is prohibited.
+
+Modifying an iterator to the extended category *random_access_iterator_tag* can be a good practice task.
